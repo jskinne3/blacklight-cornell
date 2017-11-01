872f90205862e0ef70b19a17559cf5755c3cf074
Rick Silterra <es287@cornell.edu>
2017-11-01 13:20:25 -0400

    work on showing bookbag only when redis env enabled.

--- app/models/bookbag.rb
+++ app/models/bookbag.rb
@@ -3,9 +3,28 @@ class Bookbag
 
   attr_accessor :bagname
 
+  if ENV['BAG_REDIS_HOST'] 
+    @@r = Redis.new(
+             port: ENV['BAG_REDIS_PORT'], 
+             host: ENV['BAG_REDIS_HOST'],
+             db: ENV['BAG_REDIS_DB'] )
+      @@bagname = nil 
+  else
+    @@r = nil
+  end
+
   def initialize(bagname) 
-    @r = Redis.new(port: 6379)
+    @r = @@r
     @bagname = bagname unless bagname.nil?
+    @@bagname = bagname unless bagname.nil?
+  end
+  
+  def self.enabled?
+   return !@@r.nil?
+  end
+
+  def enabled?
+   return !@r.nil?
   end
 
   def create(value)
@@ -26,4 +45,8 @@ class Bookbag
   end
 
 end
+
+#BAG_REDIS_HOST=elasticache-001.internal.library.cornell.edu
+#BAG_REDIS_PORT=6379
+#BAG_REDIS_DB=2
 
